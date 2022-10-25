e726f840283722847f667f4bae65ef687ecacf69
Daniel Carmona <carmonadacg@hotmail.com>
2022-10-24 22:54:40 -0500

    commit inicial

---
+++ Proyectociclo4/supermarket/src/components/create_product.component.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+
+export default class CreatedProduct extends Component{
+    render(){
+        return <div>create_product.component</div>;
+    }
+}
---
+++ Proyectociclo4/supermarket/src/components/edit_product.component.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+
+export default class EditProduct extends Component{
+    render(){
+        return <div>edit_product.component</div>;
+    }
+}
