54154e03eb7a5762b497c0bc3a52fbbb34cd259d
Daniel Carmona <carmonadacg@hotmail.com>
2022-11-02 21:10:03 -0500

    31 oct

--- Proyectociclo4/supermarket/src/components/create_product.component.js
+++ Proyectociclo4/supermarket/src/components/create_product.component.js
@@ -5,7 +5,28 @@ import Button from "react-bootstrap/Button";
 export default class CreatedProduct extends Component{
     render(){
         return(
-        <div>create_product.component</div>
+        <div class="form-wrapper">
+            <Form>
+                <Form.Group controlId="Codigo">
+                    <Form.Label>Codigo</Form.Label>
+                    <Form.Control type="text"/>
+                </Form.Group>
+                <Form.Group controlId="Name">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="text"/>
+                </Form.Group>
+                <Form.Group controlId="Categoria">
+                    <Form.Label>Categoria</Form.Label>
+                    <Form.Control type="text"/>
+                </Form.Group>
+                <Form.Group controlId="Cantidad">
+                    <Form.Label>Cantidad</Form.Label>
+                    <Form.Control type="text"/>
+                </Form.Group>
+                
+                <Button variant="danger" size="lg" block="block" type="submit">Crear Producto</Button>
+            </Form>
+        </div>
         );
     }
 }
